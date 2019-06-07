31af31539c3944bc4d91c4729b0ed66c9978331e
ruth <hugthebluebow@gmail.com>
2019-06-07 20:26:10 +0000

    Added setMapDimensions func. Function inserts .window rule to create columns and rows. Commented template columns and rows in stylesheet.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var defaultWidth = 8;
-var defaultHeight = 6;
+var defaultWidth = 10;
+var defaultHeight = 10;
 
 console.log("Hello World");
 
@@ -14,9 +14,18 @@ var sheet = (function() {
 	return style.sheet;
 })();
 
+//Uses variables defaultWidth and defaultHeight to set dimensions of grid
+var setMapDimensions = (function(){
+	var columns = "grid-template-columns: repeat("+defaultWidth+", 1fr);";
+	var rows = "grid-template-rows: repeat("+defaultHeight+", 1fr);";
+	
+	//var tester = "background-color: pink;";
+	sheet.insertRule(".window{"+columns+rows+"}");
+})();
+
 var cascadeIndex = 0
 
-for (var i = 1; i <= defaultWidth; i++) {
+for (var i = 2; i <= defaultWidth; i++) {
 	var index = i-1;
 	console.log(".c" + i + "{grid-column:" + i + ";}", cascadeIndex);
 	sheet.insertRule(".c" + i + "{grid-column:" + i + ";}", cascadeIndex);
